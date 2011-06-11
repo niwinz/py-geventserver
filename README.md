1f34e6a4ddf3710c6d11c6a729656ad5dae651d8
Andrei Antoukh <niwi@niwi.be>
2011-06-11 12:56:31 +0200

    Import project. Split from niwi-web.

---
+++ README.md
@@ -0,0 +1,7 @@
+py-geventserver
+===============
+
+**Description:** Script for launch any wsgi or django application over gevent wsgi server.
+**Doc:** TODO
+
+**Current version:** v5
---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import sys
+
+setup(
+    name='geventserver',
+    description='Script for launch any wsgi or django application over gevent wsgi server.',
+    author='Andrei Antoukh',
+    author_email='niwi@niwi.be',
+    url='',
+    version='5',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 5 - Stable',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+    ],
+    scripts=['geventserver.py'],
+)
